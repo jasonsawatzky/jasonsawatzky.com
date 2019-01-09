2c95c3189de1e96fabd1b845742d0c68fd8543fd
Jason Sawatzky <jasonmsawatzky@gmail.com>
2019-01-08 22:54:19 -0800

    Add github link and icon for md-graphql-spec

--- src/components/MdGraphQLSpec.jsx
+++ src/components/MdGraphQLSpec.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Tool from './Tool'
+import githubIcon from '../images/github.png'
 
 export default function({ close }) {
   return (
     <Tool close={close}>
       <h2>Markdown GraphQL Specification Generator</h2>
-      <h3>md-graphql-spec</h3>
+      <a href="https://www.github.com/jasonsawatzky/md-graphql-spec" target="_blank"  rel="noopener noreferrer">
+        <h3>md-graphql-spec <img className="twitter" src={githubIcon} width="40" alt="twitter"/></h3>
+      </a>
       <p>
         A tool to generate documentation for GraphQL APIs in Markdown format
       </p>
